b4251de0428677d16dade2a62551946613e307d3
Riwandi <riwandy10@gmail.com>
2025-03-19 14:31:35 +0700

    Initial Commit

---
+++ ResepIndonesiaApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ResepIndonesiaApp
+//
+//  Created by Riwandi on 19/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ResepIndonesiaApp/ResepIndonesiaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResepIndonesiaAppApp.swift
+//  ResepIndonesiaApp
+//
+//  Created by Riwandi on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ResepIndonesiaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
